32835f38691c030deb165599cbacf8cf978189e6
anthonychao <anthonychao@hahow.in>
2022-08-14 18:21:31 +0800

    feat(model): add user model and basic validations

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+
+  PASSWORD_FORMAT = /
+    \A
+    (?=.*?\d)           # Must contain a digit
+    (?=.*?[a-zA-Z])     # Must contain a character
+    .{8,}               # Must contain at least 8 characters
+    \z
+  /x.freeze
+
+  validates :name, presence: true
+  validates :password, format: { with: PASSWORD_FORMAT, message: :malformed_password },
+                       unless: proc { |user| user.password.nil? }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
+end
