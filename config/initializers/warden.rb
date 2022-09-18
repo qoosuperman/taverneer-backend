a1c437b9326fa07e167b53e8e257675334916e98
anthonychao <qoosuperman@gmail.com>
2022-09-18 17:02:48 +0800

    style: fix style

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -2,16 +2,18 @@
 
 Warden::Strategies.add(:password, Authentication::PasswordStrategy)
 
-Warden::Manager.serialize_into_session do |user|
-  user.id
-end
+Warden::Manager.serialize_into_session(&:id)
 
 Warden::Manager.serialize_from_session do |id|
   User.find(id)
 end
 
-module Warden::Mixins::Common
-  def request
-    @request ||= ActionDispatch::Request.new(env)
+module Warden
+  module Mixins
+    module Common
+      def request
+        @request ||= ActionDispatch::Request.new(env)
+      end
+    end
   end
 end
