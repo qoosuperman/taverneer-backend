9ecd5a767a89a96ddf6ed59fa7f382e371196434
anthonychao <qoosuperman@gmail.com>
2022-10-30 16:12:18 +0800

    chore: fix commet

--- app/models/step.rb
+++ app/models/step.rb
@@ -7,7 +7,7 @@ class Step < ApplicationRecord
 
   # NOTE: 同一個 recipe 每個 step position 需要 unique
   # 但可能上一個 step 正在改 position，下一個 position 還沒改因此重複
-  # 所以 db layer 不加上 unique index 只在 db layer 做
+  # 所以 db layer 不加上 unique index 只在 model layer 做
   # rubocop:disable Rails/UniqueValidationWithoutIndex
   validates :position, uniqueness: { scope: :recipe_id }
   # rubocop:enable Rails/UniqueValidationWithoutIndex
