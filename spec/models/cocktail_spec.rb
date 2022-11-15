8dce807c55559c26a680c274f5e458a1071626b0
anthonychao <qoosuperman@gmail.com>
2022-11-15 22:32:46 +0800

    style: fix

--- spec/models/cocktail_spec.rb
+++ spec/models/cocktail_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Cocktail, type: :model do
     subject { cocktail.twists }
 
     let(:cocktail) { create(:cocktail) }
-    let!(:child_cocktail1) { create(:cocktail, parent: cocktail) }
-    let!(:child_cocktail2) { create(:cocktail, parent: cocktail) }
+    let!(:child_cocktail_1) { create(:cocktail, parent: cocktail) }
+    let!(:child_cocktail_2) { create(:cocktail, parent: cocktail) }
 
     it 'behaves the same as descendants' do
-      expect(subject).to match([child_cocktail1, child_cocktail2])
+      expect(subject).to match([child_cocktail_1, child_cocktail_2])
     end
   end
 end
